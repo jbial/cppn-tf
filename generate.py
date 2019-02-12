70cd70e342fd57339de034e2ad8af20bb1dd347a
jbl <jbl4@illinois.edu>
2019-02-12 17:26:12 -0600

    update generate

--- generate.py
+++ generate.py
@@ -20,31 +20,25 @@ def main():
     parser.add_argument("--scale", type=float, default=1.0, help="Scaling factor for the images.")
     parser.add_argument("--name", type=str, default=None, help="Name of image for saving. Default to None for no saving." )
     parser.add_argument("--frames", type=int, default=None, help="Number of frames for the gif.")
-    parser.add_argument("--save-gif", action='store_true', help="Save a gif")
-    parser.add_argument("--save-ims", action='store_true', help="Save images")
     args = parser.parse_args()
                     
     # Init the model
     model = CPPN(im_size=args.im_size, batch_size=args.batch_size, n_dim=args.n_dim, z_dim=args.z_dim,
                 layers=args.layers, channels=args.channels, scale=args.scale)
 
-    # Show stuff
-    if not args.save_gif:
-        images = model()
-        if args.batch_size > 1:
-            vis.show_images(images, np.sqrt(images.shape[0]).astype(int))
-        else:
-            vis.show_image(images)
-        
-        # Save if specified
-        if args.name:
-            vis.save_image(images, args.name)
-        if args.save_ims:
-            vis.save_images(images)
-    else:
-        if not args.name:
-            raise ValueError("Name for gif file must not be None.")
+    # Save gif if specified
+    if args.frames and args.name:
         vis.generate_gif(model, frames=args.frames, size=args.z_dim, name=args.name)
+        print("Saved gif as {}".format(args.name))
+        raise SystemExit
 
-                        
+    # Show and save images
+    images = model()
+    if args.batch_size > 1:
+        vis.show_images(model(), np.sqrt(images.shape[0]).astype(int))
+        if args.name: vis.save_images(images)
+    else:
+        vis.show_image(model())
+        if args.name: vis.save_image(images, args.name)
+
 if __name__ == '__main__': main()
